4b38a895eb87d869a34961fa3d7d7fc886ca1478
Wesley Aerts <wesleyjan.aerts@gmail.com>
2020-05-12 11:49:39 +0200

    Init commit

---
+++ DotNetExamenJuni/NewActWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DotNetExamenJuni
+{
+    /// <summary>
+    /// Interaction logic for NewActWindow.xaml
+    /// </summary>
+    public partial class NewActWindow : Window
+    {
+        public NewActWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void BandRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (typeCountLabel != null)
+            {
+                typeCountLabel.Content = "Members";
+            }
+
+        }
+
+        private void SoloRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (typeCountLabel != null)
+            {
+                typeCountLabel.Content = "Type of artist";
+            }
+                
+        }
+    }
+}
---
+++ DotNetExamenJuni/StageLineUpWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DotNetExamenJuni
+{
+    /// <summary>
+    /// Interaction logic for StageLineUpWindow.xaml
+    /// </summary>
+    public partial class StageLineUpWindow : Window
+    {
+        public StageLineUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        //TODO: zorg dat onderstaande code werkt, volgens richtlijnen opgave
+        public void DisableEvents()
+        {
+            throw new NotImplementedException("staat hier code?");
+        }
+
+        //TODO: zorg dat onderstaande code werkt, volgens richtlijnen opgave
+        public void EnableEvents()
+        {
+            throw new NotImplementedException("staat hier code?");
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO:hier het juist item verwijderen uit de lijst.
+
+            //tip om de getoonde lijst te updaten.
+            performListBox.Items.Refresh();
+        }
+    }
+}
